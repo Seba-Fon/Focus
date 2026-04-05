ede8b038cf83a7f0e85e90a4bc037ded77b16d43
Seba-Fon <sebastian.fonseca@correo.ucu.edu.uy>
2026-04-05 19:45:07 -0300

    Se agrego funcion de guardar tareas al apretar boton

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,33 @@
-const boton = document.querySelector(".agregar-tarea");
+const contenedor = document.querySelector(".header-tareas")
 const modal = document.querySelector(".modal-tarea");
-const cancelar = document.querySelector(".cancelar");
 
+//Boton agregar tarea
+const boton = document.querySelector(".agregar-tarea");
 boton.addEventListener("click",() =>{
     modal.classList.add("activo");
 })
 
+//Boton cancelar
+const cancelar = document.querySelector(".cancelar");
 cancelar.addEventListener("click", () =>{
+    modal.classList.remove("activo");
+})
+
+//Boton guardar tarea
+const input = document.querySelector(".nombre-tarea");
+const guardar = document.querySelector(".guardar");
+
+guardar.addEventListener("click" , () =>{
+    const texto = input.value;
+    if (texto === "") return 
+
+    let tarea = document.createElement("li");
+    tarea.textContent = texto;
+
+    const lista = document.querySelector(".lista-tareas");
+    lista.appendChild(tarea);
+
+    input.value = "";
+
     modal.classList.remove("activo");
 })
